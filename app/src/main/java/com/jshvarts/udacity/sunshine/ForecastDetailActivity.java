5f2dcac2675137f95727ad5cc1c5f6036924bff0
James Shvarts <james.shvarts@viacom.com>
2015-12-06 02:44:51 -0500

    initial add settings

--- app/src/main/java/com/jshvarts/udacity/sunshine/ForecastDetailActivity.java
+++ app/src/main/java/com/jshvarts/udacity/sunshine/ForecastDetailActivity.java
@@ -1,7 +1,9 @@
 package com.jshvarts.udacity.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class ForecastDetailActivity extends AppCompatActivity {
     public static final String EXTRA_DETAIL = "detail";
@@ -10,5 +12,15 @@ public class ForecastDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast_detail);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/jshvarts/udacity/sunshine/ForecastListActivity.java
+++ app/src/main/java/com/jshvarts/udacity/sunshine/ForecastListActivity.java
@@ -1,9 +1,11 @@
 package com.jshvarts.udacity.sunshine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ForecastListActivity extends AppCompatActivity {
 
@@ -18,5 +20,15 @@ public class ForecastListActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
